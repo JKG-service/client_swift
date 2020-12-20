7fa4d058ecd1a2f800894ffd1bb2b6bfd71a4051
jeongyeonkim <yeon8480@gmail.com>
2020-12-20 13:31:15 +0900

    [#1] Login Layout Design 01 - #1

---
+++ JKG_newLimited/JKG_newLimitedApp.swift
@@ -0,0 +1,17 @@
+//
+//  JKG_newLimitedApp.swift
+//  JKG_newLimited
+//
+//  Created by 김정연 on 2020/12/14.
+//
+
+import SwiftUI
+
+@main
+struct JKG_newLimitedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
